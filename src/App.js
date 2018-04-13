7fee5734d228c9938ad36f626a4d41a4c72ce73d
Jeremy Greer <jeremy.greer.atl@gmail.com>
2018-04-12 21:24:05 -0400

    Move target to align with crosshairs

--- src/App.js
+++ src/App.js
@@ -8,18 +8,70 @@ const Row = () => (
   </div>
 )
 
+class Crosshairs extends Component {
+  state = {
+    x: 0,
+    y: 0,
+  }
+
+  handleMouseDown = (event) => {
+    this.setState({ dragging: true })
+  }
+
+  handleMouseUp = (event) => {
+    this.x = undefined
+    this.setState({ dragging: false })
+  }
+
+  handleMouseMove = (event) => {
+    if (this.state.dragging) {
+      if (this.x !== undefined) {
+        const mx = event.pageX - this.x
+        const my = event.pageY - this.y
+        this.setState({
+          x: this.state.x + mx,
+          y: this.state.y + my,
+        })
+      }
+      this.x = event.pageX
+      this.y = event.pageY
+    }
+  }
+
+  render() {
+    const { imageSource } = this.props
+    const style = {
+      backgroundImage: `url(${imageSource})`,
+      backgroundPositionX: `${this.state.x}px`,
+      backgroundPositionY: `${this.state.y}px`,
+    }
+    return (
+      <div
+        className="Crosshairs"
+        style={style}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+        onMouseMove={this.handleMouseMove}
+      >
+        <Row />
+        <Row />
+      </div>
+    )
+  }
+}
+
+
 class App extends Component {
   render() {
+    const imageSource = "https://s3.envato.com/files/72225897/target.jpg"
     return (
       <div className="App">
-        <img src="https://s3.envato.com/files/72225897/target.jpg" />
-        <div className="crosshairs">
-          <Row />
-          <Row />
-        </div>
+        <Crosshairs
+          imageSource={imageSource}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default App
